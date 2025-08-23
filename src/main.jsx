19715b0a2b3b6163fa599d0477925d2c710700a8
Sahithi Allegapally <148424406+SahithiA17@users.noreply.github.com>
2025-08-23 18:59:46 +0530

    orders Component

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
-import Temp from './Temp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Temp />
+    <App />
   </StrictMode>,
 )
