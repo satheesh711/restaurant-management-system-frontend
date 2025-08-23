898ad6bc4b79a5c6615a709e21a01fefeff2acf8
Sahithi Allegapally <148424406+SahithiA17@users.noreply.github.com>
2025-08-23 18:59:15 +0530

    order component

--- src/Temp.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css'
-import OrderForm from './components/OrderForm';
-
-export default function Temp(){
-    return(
-<OrderForm/>
-    );
-}
