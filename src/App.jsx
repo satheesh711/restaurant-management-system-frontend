9661dfbfc57ec2500fb7e9161647acd677ab6033
charan11368 <charancharan11368@gmail.com>
2025-08-24 20:24:12 +0530

    admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import React from "react";
 import './App.css'
 import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+function StaffPage() {
+  return <h2 className="text-center mt-10 text-xl">Staff Page</h2>;
 }
 
-export default App
+function ItemsPage() {
+  return <h2 className="text-center mt-10 text-xl">Items Page</h2>;
+}
+function App() {
+  return (  
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminPage />} />
+        <Route path="/staff" element={<StaffPage />} />
+        <Route path="/items" element={<ItemsPage />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
