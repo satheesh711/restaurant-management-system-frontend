70c7f434e5ec9b4c1da1da6bb5fe0d7bced2396f
Datla Rohit Varma <visaal1582892@gmail.com>
2025-08-24 17:50:50 +0530

    fixed a small bug in authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,6 +4,7 @@ import api from "../config/axiosConfig"
 export const login =async (userDetails)=>
 {
    const res= await api.post("/auth/login", userDetails, { withCredentials: true });
+   const {token,role}=res.data.data;
    localStorage.setItem("token",token)
    localStorage.setItem("role",role)
 }
